4159a6b5b0bbd9199884f5487b7b5a9393d1cbe9
echo 'hello world <ssmyth1071@gmail.com>
2018-01-02 12:08:05 -0500

    update charts on stock show. remove uniqueness validation

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,7 +1,7 @@
 class Stock < ApplicationRecord
   belongs_to :account
   belongs_to :user
-  validates :ticker, uniqueness: true
+  # validates :ticker, uniqueness: true
   validates :ticker, presence: true, length: { maximum: 6 }
   before_save :upcase_fields
 
