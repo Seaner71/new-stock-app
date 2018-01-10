9be079a3476431d87c1378bb136495e792061c7c
echo 'hello world <ssmyth1071@gmail.com>
2018-01-09 21:02:55 -0500

    add base code for revenue and expense charts:

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -27,8 +27,19 @@ class Stock < ApplicationRecord
      response = RestClient.get("https://api.iextrading.com/1.0/stock/#{self.ticker}/logo")
      return logo = JSON.parse(response.body)['url']
    end
+   # performs the GET request to get the 5 most recent news stories from IEX
    def get_news
      response = RestClient.get("https://api.iextrading.com/1.0/stock/#{self.ticker}/news/last/5")
      JSON.parse(response.body)
    end
 end
+
+## Format for Revenue expense charts - will use on Stock show when get the data in
+
+=begin
+<%= bar_chart [
+  {name: "Revenue", data: {"2017-09-30"=>52579000000, "2017-06-30"=>45408000000, "2017-03-31"=>52896000000, "2016-12-31"=>78351000000}},
+  {name: "Expenses", data: {"2017-09-30"=>32579000000, "2017-06-30"=>25408000000, "2017-03-31"=>22896000000, "2016-12-31"=>48351000000}},
+  {name: "Net Income", data: {"2017-09-30"=>22579000000, "2017-06-30"=>25408000000, "2017-03-31"=>32896000000, "2016-12-31"=>38351000000}}]
+  %>
+=end
